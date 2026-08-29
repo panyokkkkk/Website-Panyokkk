a15e75d82c69917c09ab9f29dfe95f4a66503e24
panyokkkkk <panyokkkkk@gmail.com>
2026-08-29 10:01:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,12 @@
-const texts = [
-    "Web Developer Pemula",
-    "Siswa XII RPL 1",
-    "HTML • CSS • JavaScript • PHP • Git"
-];
-
-let textIndex = 0;
-let charIndex = 0;
-
-const typing = document.getElementById("typing");
-
-function typeText(){
-
-    if(charIndex < texts[textIndex].length){
-
-        typing.textContent += texts[textIndex].charAt(charIndex);
-
-        charIndex++;
-
-        setTimeout(typeText,80);
-
-    }else{
-
-        setTimeout(deleteText,1500);
-
-    }
-
-}
-
-function deleteText(){
-
-    if(charIndex > 0){
-
-        typing.textContent =
-        texts[textIndex].substring(0,charIndex-1);
-
-        charIndex--;
-
-        setTimeout(deleteText,40);
-
-    }else{
-
-        textIndex++;
-
-        if(textIndex >= texts.length){
-            textIndex = 0;
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
         }
-
-        setTimeout(typeText,300);
-
-    }
-
-}
-
-typeText();
-
-const observer = new IntersectionObserver(entries => {
-
-entries.forEach(entry => {
-
-if(entry.isIntersecting){
-
-entry.target.classList.add("show");
-
-}
-
+    });
 });
 
-});
-
-document.querySelectorAll("section").forEach(section => {
-
-section.classList.add("hidden");
-
-observer.observe(section);
-
+document.querySelectorAll("section").forEach((el)=>{
+    el.classList.add("hidden");
+    observer.observe(el);
 });
